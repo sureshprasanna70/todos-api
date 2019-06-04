d4f7bb0ddd775458a2599d7bf8438de7e0f22c7c
suresh prasanna <sureshprasanna70@gmail.com>
2019-06-04 22:37:43 +0530

    todos controller with request spec

---
+++ spec/requests/todos_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe 'Todos', type: :request do
+  let!(:todos) { create_list(:todo, 10) }
+  let (:todo_id) { todos.first.id }
+
+  describe 'GET todos/' do
+    before { get '/todos' } 
+
+    it 'returns todos' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+
+  end
+
+  describe 'POST /todos' do
+    let(:valid_attributes) { { title: 'Learn Elm', created_by: '1' } }
+
+    context 'when the request is valid' do
+      before { post '/todos', params: valid_attributes }
+
+      it 'creates a todo' do
+        expect(json['title']).to eq('Learn Elm')
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when the request is invalid' do
+      before { post '/todos', params: { title: 'Foobar' } }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body).to match(/Validation failed: Created by can't be blank/)
+      end
+    end
+  
+  end
+
+end
