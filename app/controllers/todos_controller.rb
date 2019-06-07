64ee3fe0b35a409a974db482e447702190587fad
suresh prasanna <sureshprasanna70@gmail.com>
2019-06-07 19:04:58 +0530

    add show, create and destroy

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,10 +6,24 @@ class TodosController < ApplicationController
     @todos = Todo.all
     json_response(@todos)
   end
- 
+
   def create
     todo = Todo.create!(todo_params)
     json_response(todo,:created)
+  end
+
+  def update
+    @todo.update(todo_params)
+    head :no_content
+  end
+
+  def show
+    json_response(@todo)
+  end
+
+  def destroy
+    @todo.destroy
+    head :no_content
   end
 
   private
